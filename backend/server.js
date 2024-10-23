040e951c0bef390f1ac21954a73f3717578414df
Adil Shaikh <89292349+adilshaikh19@users.noreply.github.com>
2024-10-23 09:54:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: "*"
+}))
 
 mongoose
   .connect(process.env.MONGO_URI, {
